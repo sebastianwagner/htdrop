3f52ed14aeeeae9ab4647ac9d14b9a8be439dac9
Sebastian Wagner <2000sw@gmail.com>
2014-10-23 00:50:37 +0200

    more headers

--- recieve.js
+++ recieve.js
@@ -4,12 +4,16 @@
 var http = require('http'),
       qs = require('querystring');
 
-function leForm(res) {
+function leForm(res, timeout) {
+ res.setHeader("Cache-Control", 'max-age=' + 1 * 60 * 60);
+ res.setHeader("Keep-Alive", 'timeout=' + timeout);
+ res.setHeader("Content-Type", 'text/html; charset=utf-8');
  res.writeHead(200);
  res.end('<!DOCTYPE html>' + "\n" + '<form method="post"><input type="file" name="filez" multiple required><input type="submit"></form>');
 }
 
 var server = http.createServer(function(req, res) {
+  var timeout = this.timeout / 1000;
   switch(req.method) {
     case 'POST':
      var weGotData = false, body = '';
@@ -23,11 +27,12 @@ var server = http.createServer(function(req, res) {
         var post = qs.parse(body);
         console.log(post);
       }
-      leForm(res);
+      leForm(res, timeout);
      });
      break;
     default:
-     leForm(res);
+     leForm(res, timeout);
   }
 });
-server.listen(8080);
+var instance = server.listen(8080);
+
