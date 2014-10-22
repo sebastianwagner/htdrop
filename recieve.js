707a45c16e691f91be0d3de193cfb792ba653918
Sebastian Wagner <2000sw@gmail.com>
2014-10-22 22:54:35 +0200

    distinguish POST

--- recieve.js
+++ recieve.js
@@ -3,8 +3,25 @@
  */
 var http = require('http');
 
+function leForm(res) {
+ res.writeHead(200);
+ res.end('<!DOCTYPE html>' + "\n" + '<form method="post"><input type="file" name="filez" multiple required><input type="submit"></form>');
+}
+
 var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('<form method="post"><input type="file"><input type="submit"></form>');
+  switch(req.method) {
+    case 'POST':
+     req.on('data', function(data){
+      console.log(data);
+      console.log('we gotz data!');
+     }).on('end', function(end){
+      console.log('omg the end is nea..');
+      leForm(res);
+     });
+     
+     break;
+    default:
+     leForm(res);
+  }
 });
 server.listen(8080);
