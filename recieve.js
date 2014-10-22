da3132b987269d118f41a5ac902720e1ddde245c
Sebastian Wagner <2000sw@gmail.com>
2014-10-22 22:23:09 +0200

    upload

--- recieve.js
+++ recieve.js
@@ -1,7 +1,10 @@
+/**
+ * @see http://nodeguide.com/beginner.html#a-hello-world-http-server
+ */
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('Hello Http');
+  res.end('<form method="post"><input type="file"><input type="submit"></form>');
 });
 server.listen(8080);
