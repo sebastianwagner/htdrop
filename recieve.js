23e502a490901df3164bd8f54454fb1e7d206283
Sebastian Wagner <2000sw@gmail.com>
2014-10-23 02:07:57 +0200

    formidable

--- recieve.js
+++ recieve.js
@@ -2,21 +2,39 @@
  * @see http://nodeguide.com/beginner.html#a-hello-world-http-server
  */
 var http = require('http'),
+formidable = require('formidable'),
+util = require('util'),
       qs = require('querystring');
 
 function leForm(res, timeout) {
- res.setHeader("Cache-Control", 'max-age=' + 1 * 60 * 60);
+ res.setHeader("Cache-Control", 'max-age=' + 1 * 60 * 60 + ', public');
  res.setHeader("Keep-Alive", 'timeout=' + timeout);
  res.setHeader("Content-Type", 'text/html; charset=utf-8');
  res.writeHead(200);
- res.end('<!DOCTYPE html>' + "\n" + '<form method="post"><input type="file" name="filez" multiple required><input type="submit"></form>');
+ res.end('<!DOCTYPE html>' + "\n" + '<form method="post" action="?upload" enctype="multipart/form-data"><input type="file" name="filez" multiple required><input type="submit"></form>');
+}
+
+function leLink(res) {
+    res.setHeader("Connection", 'close');
+    res.setHeader("Cache-Control", 'max-age=' + 1 * 60 * 60+ ', public');
+    res.setHeader("Content-Type", 'text/html; charset=utf-8');
+    res.writeHead(200);
+    res.write('<!DOCTYPE html>' + "\n" + '<a href="/">upload</a>');
+    //res.end(util.inspect({fields: fields, files: files}));
+    //close TCP-connection proactively
+    res.connection.end();
 }
 
 var server = http.createServer(function(req, res) {
   var timeout = this.timeout / 1000;
   switch(req.method) {
     case 'POST':
+     var form = new formidable.IncomingForm();
+     form.parse(req, function(err, fields, files) {
+      leLink(res);
+     });
      var weGotData = false, body = '';
+     break;
      req.once('data', function(chunk){
       weGotData = true;
       body += chunk;
